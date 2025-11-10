09dae38cd369b076901d86448ad7948d85991689
nharshitha29 <nharshitha29@gmail.com>
2025-11-10 17:33:34 +0530

    Update network1.tf

--- network1.tf
+++ network1.tf
@@ -1,3 +1,8 @@
+--------------------------------
+approach - 2
+-------------------------------
+
+
 resource "aws_vpc" "base" {
   cidr_block = var.vpc_info.cidr
   tags       = var.vpc_info.tags
@@ -74,3 +79,102 @@ resource "aws_route_table_association" "private_db" {
   subnet_id      = aws_subnet.db.id
 
 }
+
+----------------------------------------
+approach - 3
+----------------------------------------
+resource "aws_vpc" "base" {
+  cidr_block = var.vpc_info.cidr
+  tags       = var.vpc_info.tags
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_subnet" "public" {
+  count             = length(var.public_subnet)
+  vpc_id            = aws_vpc.base.id
+  availability_zone = var.public_subnet[count.index].az
+  cidr_block        = var.public_subnet[count.index].cidr
+  tags              = var.public_subnet[count.index].tags
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet)
+  vpc_id            = aws_vpc.base.id
+  availability_zone = var.private_subnet[count.index].az
+  cidr_block        = var.private_subnet[count.index].cidr
+  tags              = var.private_subnet[count.index].tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.base.id
+  tags = {
+    Name = "manual"
+    Env  = "Dev"
+
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.base.id
+  tags = {
+    Name = "private"
+    Env  = "Dev"
+
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_subnet.private]
+}
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.base.id
+  tags = {
+    Name = "public"
+    Env  = "Dev"
+
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_internet_gateway.igw, aws_subnet.public]
+}
+
+resource "aws_route" "route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  depends_on = [aws_internet_gateway.igw, aws_route_table.public]
+
+
+}
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnet)
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public[count.index].id
+}
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet)
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private[count.index].id
+
+}
+
