6ddb3aef43c3d7b52c869b9861965634f1504309
Harshitha <nharshitha29@gmail.com>
2025-11-25 10:23:24 +0530

    added changes

--- terraform-1/aws_ntier/inputs1.tf
+++ terraform-1/aws_ntier/inputs1.tf
@@ -125,3 +125,29 @@ variable "private_subnet" {
   description = "private subnet"
 
 }
+variable "vpc_id" {
+  type = string
+}
+variable "security_group" {
+    type = object({
+      name = string
+      tags = map(string)
+    })
+  
+}
+variable "ingress_rules" {
+  type = list(object({
+    cidr_ipv4  = string
+    from_port = number
+    ip_protocol =string
+    to_port = number
+  }))
+  
+}
+variable "egress_rules" {
+  type =list(object({
+    cidr_ipv4 = string
+    ip_protocol = string
+  }))
+  
+}
--- terraform-1/aws_ntier/network.tf
+++ terraform-1/aws_ntier/network.tf
@@ -89,3 +89,24 @@ resource "aws_route_table_association" "private_db" {
   subnet_id      = aws_subnet.db.id
 
 }
+resource "aws_security_group" "base" {
+  
+  vpc_id = var.vpc_id
+  name = var.security_group.name
+  tags = var.security_group.tags
+}
+resource "aws_vpc_security_group_ingress_rule" "web_rules" {
+    count = length(var.ingress_rules)
+    security_group_id =aws_security_group.base.id
+    cidr_ipv4         =   var.ingress_rules[count.index].cidr_ipv4      
+    from_port         =var.ingress_rules[count.index].from_port
+    ip_protocol       = var.ingress_rules[count.index].ip_protocol
+    to_port           = var.ingress_rules[count.index].to_port
+}
+
+resource "aws_vpc_security_group_egress_rule" "web_rules" {
+  count = length(var.egress_rules)
+  security_group_id = aws_security_group.base.id
+  cidr_ipv4         = var.egress_rules[count.index].cidr_ipv4
+  ip_protocol       = var.egress_rules[count.index].ip_protocol
+}
