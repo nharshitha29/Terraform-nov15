364dedd1ebfb114c6e884a514c4c38ab74f31898
Harshitha <nharshitha29@gmail.com>
2025-11-29 16:57:38 +0530

    c12

--- terraform/aws_ntier/network.tf
+++ terraform/aws_ntier/network.tf
@@ -147,7 +147,7 @@ data "aws_ami" "ubuntu_primary" {
 resource "aws_instance" "base" {
     count = length(var.public_subnet)
     ami = data.aws_ami.ubuntu_primary.id
-    subnet_id = aws_subnet.public[count.index].id[0] 
+    subnet_id = aws_subnet.public[count.index].id
     associate_public_ip_address = true
     instance_type = var.instance_type
     key_name = aws_key_pair.primary.key_name
