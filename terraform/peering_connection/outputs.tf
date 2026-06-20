8be5834ffa45da96b2b538493f2102f47c44aa9d
Harshitha <nharshitha29@gmail.com>
2026-06-20 16:01:08 +0530

    natgateway is added

---
+++ terraform/peering_connection/outputs.tf
@@ -0,0 +1,18 @@
+output "ipaddress" {
+  value = aws_instance.webservser.public_ip
+
+}
+output "ipaddress1" {
+  value = aws_instance.webservser1.private_ip
+
+}
+
+
+output "ssh_path" {
+  value = format("ssh %s@%s", var.instance.username, aws_instance.webservser.public_ip)
+
+}
+output "ssh_path1" {
+  value = format("ssh %s@%s", var.instance.username, aws_instance.webservser1.private_ip)
+
+}
